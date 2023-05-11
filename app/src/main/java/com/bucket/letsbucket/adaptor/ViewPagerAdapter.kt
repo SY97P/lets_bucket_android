e87ed648f837f7efe8fea3156c253092baa6d277
SY97P <onetuks@naver.com>
2023-05-12 00:23:55 +0900

    viewpager callback func added

--- app/src/main/java/com/bucket/letsbucket/adaptor/ViewPagerAdapter.kt
+++ app/src/main/java/com/bucket/letsbucket/adaptor/ViewPagerAdapter.kt
@@ -7,16 +7,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bucket.letsbucket.R
 import com.bucket.letsbucket.data.DatePage
 import com.bucket.letsbucket.databinding.ItemViewpagerBinding
+import com.bucket.letsbucket.fragment.ThisYearFragment
 import com.bucket.letsbucket.util.LogUtil
 
 class ViewPagerAdapter(private val listData: ArrayList<DatePage>) : RecyclerView.Adapter<ViewPagerAdapter.ViewHolderPage>() {
-    lateinit var binding: ItemViewpagerBinding
+    private val TAG = javaClass.simpleName
 
+    lateinit var binding: ItemViewpagerBinding
 
     inner class ViewHolderPage(val binding: ItemViewpagerBinding) : RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: DatePage) {
             binding.tvTitle.text = data.title
-//            binding.tvTitle.setTextSize(20f)
         }
     }
 
@@ -27,9 +28,9 @@ class ViewPagerAdapter(private val listData: ArrayList<DatePage>) : RecyclerView
 
     override fun onBindViewHolder(holder: ViewHolderPage, position: Int) {
         val viewHolder: ViewHolderPage = holder
-        viewHolder.onBind(listData[position])
+        viewHolder.onBind(listData[position % listData.size])
     }
 
-    override fun getItemCount(): Int = listData.size
+    override fun getItemCount(): Int = Int.MAX_VALUE
 
 }
--- app/src/main/java/com/bucket/letsbucket/fragment/ThisYearFragment.kt
+++ app/src/main/java/com/bucket/letsbucket/fragment/ThisYearFragment.kt
@@ -1,8 +1,5 @@
 package com.bucket.letsbucket.fragment
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,19 +8,59 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager2.widget.ViewPager2
 import com.bucket.letsbucket.R
-import com.bucket.letsbucket.activity.MainActivity
 import com.bucket.letsbucket.adaptor.ViewPagerAdapter
 import com.bucket.letsbucket.data.DatePage
 import com.bucket.letsbucket.databinding.FragmentThisyearBinding
 import com.bucket.letsbucket.util.LogUtil
+import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.properties.Delegates
 
 class ThisYearFragment : Fragment() {
 
     private var TAG: String = javaClass.simpleName
 
     private lateinit var binding: FragmentThisyearBinding
+
+    private lateinit var adapter: ViewPagerAdapter
+
+    private lateinit var calendar: Calendar
+    private var mPosition: Int by Delegates.observable(0) { property, oldValue, newValue ->
+        if (oldValue < newValue) {
+            calendar.run {
+                add(Calendar.MONTH, 1)
+                time
+            }
+            val datetime = SimpleDateFormat("yyyy년 M월", Locale.KOREA).format(calendar.time)
+            binding.calendarYearMonthText.text = datetime
+        } else if (oldValue > newValue) {
+            calendar.run {
+                add(Calendar.MONTH, -1)
+                time
+            }
+            val datetime = SimpleDateFormat("yyyy년 M월", Locale.KOREA).format(calendar.time)
+            binding.calendarYearMonthText.text = datetime
+        }
+    }
+
+    private val pageChangeCallback = object: ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            mPosition = position
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        calendar = Calendar.getInstance()
+
+        val list: ArrayList<DatePage> = arrayListOf(
+            DatePage("1 page"),
+            DatePage("2 page")
+        )
+        adapter = ViewPagerAdapter(list)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -32,12 +69,17 @@ class ThisYearFragment : Fragment() {
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_thisyear, container, false)
 
-        val list: ArrayList<DatePage> = arrayListOf(
-            DatePage("1 page"),
-            DatePage("2 page")
-        )
-        binding.viewPager.adapter = ViewPagerAdapter(list)
+        binding.viewPager.adapter = adapter
         binding.viewPager.orientation = ViewPager2.ORIENTATION_VERTICAL
+
+        setupBinding()
+
         return binding.root
+    }
+
+    private fun setupBinding() {
+        binding.let {
+            it.viewPager.registerOnPageChangeCallback(pageChangeCallback)
+        }
     }
 }
